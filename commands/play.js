549194514b1bca902f8a849e4f274de041439887
Xenon <78933353+xenoncommits@users.noreply.github.com>
2025-07-19 21:02:03 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -15,15 +15,13 @@ module.exports = {
     const query = interaction.options.getString('query');
     const voiceChannel = interaction.member.voice.channel;
     
-    // Check if user is in a voice channel
     if (!voiceChannel) {
       return interaction.reply({ 
         embeds: [errorEmbed(`${emojis.error} You need to be in a voice channel to use this command!`)], 
         ephemeral: true 
       });
     }
-    
-    // Check bot permissions
+
     const permissions = voiceChannel.permissionsFor(interaction.client.user);
     if (!permissions.has('Connect') || !permissions.has('Speak')) {
       return interaction.reply({ 
@@ -31,26 +29,19 @@ module.exports = {
         ephemeral: true 
       });
     }
-    
-    // Defer reply since playing music might take some time
+
     await interaction.deferReply();
-    
+
     try {
-      // Set textChannel for DisTube events
-      const queue = interaction.client.distube.getQueue(interaction.guildId);
-      if (!queue) {
-        interaction.client.distube.voices.join(voiceChannel);
-      }
-      
+      // Show "Searching..." message before playing
+      await interaction.editReply({ 
+        embeds: [infoEmbed(`${emojis.search} Searching for: \`${query}\``)]
+      });
+
       await interaction.client.distube.play(voiceChannel, query, {
         member: interaction.member,
         textChannel: interaction.channel,
         metadata: { interaction }
-      });
-      
-      // Edit the deferred reply
-      await interaction.editReply({ 
-        embeds: [infoEmbed(`${emojis.search} Searching for: \`${query}\``)]
       });
     } catch (error) {
       console.error(error);
@@ -59,4 +50,4 @@ module.exports = {
       });
     }
   },
-}; 
+};
